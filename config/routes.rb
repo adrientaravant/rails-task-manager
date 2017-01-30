32359632663fa5cb5d270d15ecb4d63f48e397cf
adrien taravant <adrien.taravant@gmail.com>
2017-01-30 11:20:42 +0100

    fin exo => CRUD rails + DRY

--- config/routes.rb
+++ config/routes.rb
@@ -2,38 +2,41 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :tasks
+  root "tasks#index"
   # CRUD
-  # Create = POST
-  # Read = GET
-  # Update = PATCH (or PUT)
-  # Delete = Delete
+  # # Create = POST
+  # # Read = GET
+  # # Update = PATCH (or PUT)
+  # # Delete = Delete
 
-  # == READ ==
 
-  # 1. Afficher toutes les tâches (GET)
-  get "tasks" => "tasks#index"
 
-  # 2. Afficher une tâche en particulier (GET)
-  get "tasks/:id" => "tasks#show"
+  # # == READ ==
 
-  # == CREATE ==
+  # # 1. Afficher toutes les tâches (GET)
+  # get "tasks" => "tasks#index"
 
-  # 3. Afficher la page pour saisir une nouvelle tâche (GET)
-  get "tasks/new" => "tasks#new"
+  # # 2. Afficher une tâche en particulier (GET)
+  # get "tasks/:id" => "tasks#show", as: :task
+  # # match 'tasks/:id', to: 'tasks#show', constraints: { id: /\d+/}, via: :get
 
-  # 4. Sauvegarder la nouvelle tâche dans la base (POST)
-  post "tasks" => "tasks#create"
+  # # 4. Sauvegarder la nouvelle tâche dans la base (POST)
+  # post "tasks" => "tasks#create"
 
-  # == UPDATE ==
+  # # == UPDATE ==
 
-  # 5. Afficher la page de la tâche à mettre à jour (GET)
-  get "tasks/:id/edit" => "tasks#edit"
+  # # 5. Afficher la page de la tâche à mettre à jour (GET)
+  # get "tasks/:id/edit" => "tasks#edit"
 
-  # 6. Sauvegarder les modifications de la tâche dans la base (PATCH)
-  patch "tasks/:id" => "tasks#update"
+  # # 6. Sauvegarder les modifications de la tâche dans la base (PATCH)
+  # patch "tasks/:id" => "tasks#update"
 
-  # == DELETE ==
+  # # == DELETE ==
 
-  # 7. Supprimer une tâche (DELETE)
-  delete "tasks/:id" => "tasks#destroy"
+  # # 7. Supprimer une tâche (DELETE)
+  # delete "tasks/:id" => "tasks#destroy"
+
+  # # Pour que la racine "/" du site renvoie vers tasks#index par défaut
+  # root "tasks#index"
 end
